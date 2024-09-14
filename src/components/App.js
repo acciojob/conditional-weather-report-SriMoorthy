615bdb09a2279960eae4a89f6a649730101e9db6
SriMoorthy <srimoorthy27@gmail.com>
2024-09-14 12:27:07 +0530

    Okay

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay"
+
+
+const def = {temperature : 0 , condition : "Cool"}
 
 const App = () => {
+
+  const[data,setData] = useState(def)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setData({ temperature : 25, condition : "Sunny"})
+    },1000)
+  })
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <WeatherDisplay data = {data}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WeatherDisplay = (props) => {
+     const {data} = props
+
+     const style = {
+        color : data.temperature > 20 ? 'red' : 'blue'
+     }
+
+     return (
+        <div>
+            <p>Temperature: <span style={tempStyle}>{data.temperature}</span></p>
+            <p>Conditions: {data.conditions}</p>
+        </div>
+     );
+};
+
+export default WeatherDisplay
